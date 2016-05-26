3bad1a05fba54a7c00e2f00add4a219493ed0b52
Miguel Gonzalez <fuermiguel@yahoo.es>
2016-05-26 16:27:00 +0100

    inicial funciona cargar imagenes en label

---
+++ src/logica/MostrarPlantilla.java
@@ -0,0 +1,106 @@
+/*
+ * Muestra los carteles en un jform. Coje la información del cartel de un 
+archivo xml.
+ */
+package logica;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import org.netbeans.xml.schema.cartel.TipoPlantilla;
+import pantallas.Plantilla1;
+
+
+/**
+ *
+ * @author Miguel
+ */
+public class MostrarPlantilla {
+
+    public static void main(String[] args) {
+     String fndCabecera,imgCabecera,fndPrincipal,imgPrincipal,fndPie;
+
+        try {
+
+            /* Crear una instancia de la clase JAXBContext para
+    poder manipular las clases generadas en el paquete jaxb.plantilla
+    La clase JAXBContext proporciona al cliente un punto de entrada a la API JAXB
+    Facilita una abstracción para manejar la información generada para
+    implementar las operaciones del JAXB binding framework como unmarshal y marshal
+    unmarshal: consiste en convertir datos XML en un árbol de objetos Java
+    marshal: consiste en convertir un árbol de objetos Java a datos XML*/
+            
+            
+            System.out.println("inicio mostrar plantilla");
+            JAXBContext jaxbContext = JAXBContext.newInstance("org.netbeans.xml.schema.cartel");
+            System.out.println("instancié el paquete que contiene las clases generadas");
+
+            // Crear un objeto de tipo Unmarshaller para convertir datos XML en un árbol de objetos Java
+            Unmarshaller u = jaxbContext.createUnmarshaller();
+
+            // La clase JAXBElement representa a un elemento de un documento XML
+            // en este caso a un elemento del documento plantilla.xml
+            /*JAXBElement jaxbElement = (JAXBElement) u.unmarshal(
+                               new FileInputStream(System.getProperty("user.home")
+                               + System.getProperty("file.separator") + "albaran.xml"));*/
+            
+            JAXBElement jaxbElement = (JAXBElement) u.unmarshal(
+                    new FileInputStream("C:\\Users\\Miguel\\Documents"
+                            + "\\NetBeansProjects\\Carteles1\\src\\"
+                            + "carteles\\cartel.xml"));
+            
+
+
+/* Elmétodo getValue() retorna el modelo de contenido (content model) y el valor de los atributos del elemento
+            PedidoType pedidoType = (PedidoType) jaxbElement.getValue();*/
+
+            // Obtenemos una instancia de tipo Plantilla para obtener un Objeto 
+            //de tipo Plantilla
+            TipoPlantilla cartel = (TipoPlantilla) jaxbElement.getValue();
+            // Establecemos los datos
+           
+            
+            fndCabecera = cartel.getCabecera().getFondo();
+            imgCabecera = cartel.getCabecera().getImagen();
+            fndPrincipal = cartel.getPrincipal().getFondo();
+            imgPrincipal = cartel.getPrincipal().getImagen();
+            fndPie = cartel.getPie().getFondo();
+            cartel.getPie().getSponsor();
+            
+            //Creo una platilla
+            Plantilla1 plantilla1 = new Plantilla1(fndCabecera,imgCabecera,
+            fndPrincipal,imgPrincipal,fndPie);
+            plantilla1.setVisible(true);
+
+            // Crear un objeto de tipo Marshaller para posteriormente convertir un
+            // el árbol de objetos Java a datos XML
+            Marshaller m = jaxbContext.createMarshaller();
+
+            // El método setProperty(String nombrePropiedad, Object value) recibe en este
+            // caso la propiedad "jaxb.formatted.output". Esta propiedad controla si al
+            // realizar un marshal, debe formatear el resultado XML con saltos de linea
+            // e indentaciones para que las personas podamos leerlo cómodamente. Por defecto
+            // su valor es falso es decir el XML creado no está formateado
+            // El argumento value en este caso tiene que ser concretamente de tipo Boolean
+            // para indicar si queremos que el resultado XML esté formateado o no
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+            // El método marshall(Object elementoJAXB, OutputStream os) recibe un objeto
+            // de tipo JAXBElement para que su contenido lo muestre en la salida estándar
+            // debido a que este método está sobrecargo, si miramos la documentación de
+            //la API podemos ver como podemos mostrar o escribir el resultado XML de
+            //diferentes maneras
+            m.marshal(jaxbElement, System.out);
+
+        } catch (JAXBException je) {
+            System.out.println("uno");
+            System.out.println(je.getCause());
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+    }
+}
